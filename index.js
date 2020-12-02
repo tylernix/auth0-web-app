44fb15c13aedec63a3f00cf6467659a36a7f7a54
Tyler Nix <67964959+tylernix@users.noreply.github.com>
2020-12-02 13:11:46 -0600

    Use access token to call expenses api

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.use(
    auth0Logout: true,
    baseURL: APP_URL,
    authRequired: false,
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+
  })
 );
 
@@ -74,7 +79,12 @@ app.get("/user", requiresAuth(), async (req, res) => {
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
    try {
-     const expenses = await axios.get(`${API_URL}/reports`);
+     const { token_type, access_token } = req.oidc.accessToken;
+     const expenses = await axios.get(`${API_URL}/reports`, {
+       headers: {
+         Authorization: `${token_type} ${access_token}`,
+       },
+     });
      res.render("expenses", {
        user: req.oidc && req.oidc.user,
        expenses: expenses.data,
